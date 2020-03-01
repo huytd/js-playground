5c8210e9d03b17b6e296356de3dffa17703149ea
Huy Tran <kingbazoka@gmail.com>
2020-03-01 11:28:48 -0800

    Some visual update, and fix Object rendering issue.

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ const result = bubbleSort(input);
 log(">> Result", result);
 debug(result);`;
 
-const VisualBoolean = (props) => <div className={"py-1 px-2 mb-2 bg-purple-100 text-purple-500 border border-purple-500"}> {props.value.toString()} </div>;
+const VisualBoolean = (props) => <div className={"py-1 px-2 mb-2 bg-purple-100 text-purple-500 rounded-md border border-b-2 border-purple-500"}> {props.value.toString()} </div>;
 
-const VisualNumber = (props) => <div className={"py-1 px-2 mb-2 bg-blue-100 text-blue-500 border border-blue-500"}> {props.value} </div>;
+const VisualNumber = (props) => <div className={"py-1 px-2 mb-2 bg-blue-100 text-blue-500 rounded-md border border-b-2 border-blue-500"}> {props.value} </div>;
 
 const VisualString = (props) => {
     const isMatched = (index) => {
@@ -47,7 +47,7 @@ const VisualString = (props) => {
         if (c === ' ') return '⠀'; // This is not a space
         else return c;
     });
-    return <div className={"py-1 px-2 mb-2 bg-red-100 text-red-500 border border-red-500 flex flex-row flex-wrap"}>
+    return <div className={"py-1 px-2 mb-2 bg-red-100 text-red-500 rounded-md border border-b-2 border-red-500 flex flex-row flex-wrap"}>
         {chars.length === 1 ? chars[0] : chars.map((c, i) => <div key={i} className={"pt-5 px-2 bg-red-200 m-1 relative " + (isMatched(i) ? "bg-red-400 text-white" : "")}>
             <span className={"absolute top-0 left-0 text-xs ml-1 opacity-50 " + (isMatched(i) ? "text-white" : "")}>{i}</span>
             {c}
@@ -59,9 +59,9 @@ const VisualObject = (props) => {
     const content = [];
     const obj = props.value;
     for (let key in obj) {
-        content.push(<div key={Date.now()} className={"block p-2"}><div className={"inline-block w-1/12"}>{key}:</div><VisualElement value={obj[key]}/></div>);
+        content.push(<div key={Date.now() + key} className={"block p-2"}><div className={"inline-block w-1/12"}>{key}:</div><VisualElement value={obj[key]}/></div>);
     }
-    return <div className={"py-1 px-2 mb-2 bg-orange-100 text-orange-500 border border-red-500 flex flex-col"}> {content} </div>;
+    return <div className={"py-1 px-2 mb-2 bg-orange-100 text-orange-500 rounded-md border border-b-2 border-red-500 flex flex-row"}> {content} </div>;
 };
 
 const VisualArray = (props) => {
@@ -74,8 +74,8 @@ const VisualArray = (props) => {
         }
         return false;
     };
-    return <div className={"py-1 px-2 mb-2 bg-green-100 text-green-500 border border-green-500 flex flex-row flex-wrap"}>
-        {props.value.map((c, i) => <div key={i} className={"pt-5 px-2 bg-green-200 m-1 relative " + (isMatched(i) ? "bg-green-400" : "")}>
+    return <div className={"py-1 px-2 mb-2 bg-gray-100 text-gray-500 rounded-md border border-b-2 border-gray-500 flex flex-row flex-wrap"}>
+        {props.value.map((c, i) => <div key={i} className={"pt-5 px-2 bg-gray-200 m-1 relative " + (isMatched(i) ? "bg-gray-400" : "")}>
             <span className={"absolute top-0 left-0 text-xs ml-1 opacity-50 " + (isMatched(i) ? "text-white" : "")}>{i}</span>
             <VisualElement value={c}/>
         </div>)}
@@ -104,7 +104,7 @@ const VisualElement = (props) => {
     return null;
 };
 
-const VisualLog = (props) => <div className={"p-2 mb-2 bg-gray-100 text-gray-600 border border-gray-500 block clear-both"}> {props.value.toString()} </div>;
+const VisualLog = (props) => <div className={"p-2 mb-2 bg-gray-100 text-gray-600 rounded-md border border-b-2 border-gray-500 block clear-both"}> {props.value.toString()} </div>;
 
 const useStoredState = (defaultValue, key) => {
     const [value, setValue] = React.useState(() => {
@@ -127,6 +127,7 @@ const App = () => {
 
     const handleEditorDidMount = (ref, editor) => {
         editorRef.current = ref;
+        console.log("EDITOR",editor);
     };
 
     const executeCode = () => {
@@ -188,18 +189,18 @@ const App = () => {
             />
             <div className={"bg-gray-100 h-20 p-3 border-gray-300 border-t flex flex-row"}>
                 <pre className={"flex-1 overflow-y-auto"}>{logContent.length ? logContent.join("\n") : "Press Ctrl + Enter to run the code."}</pre>
-                <button className={"m-1 px-3 bg-green-500 text-white rounded-lg"} onClick={() => executeCode()}>Run</button>
+                <button className={"m-1 px-3 bg-green-100 hover:bg-green-200 rounded-md border-green-500 text-green-700 rounded-md border border-b-2"} onClick={() => executeCode()}>Run</button>
             </div>
         </div>
         <div className={"flex-1 bg-gray-100 flex flex-col border-l border-gray-300"}>
-            <div className={"flex-1 overflow-y-auto p-3"}>
-            {result.map((entry, i) => {
-                if (typeof entry.value === 'string' && entry.value.startsWith("@LOG=")) {
-                    return <VisualLog key={i} value={entry.value.replace(/^@LOG=/, '')}/>;
-                } else {
-                    return <VisualElement key={i} value={entry.value} param={entry.param}/>;
-                }
-            })}
+            <div className={"flex-1 overflow-y-auto p-3 sketch-area"}>
+                {result.map((entry, i) => {
+                    if (typeof entry.value === 'string' && entry.value.startsWith("@LOG=")) {
+                        return <VisualLog key={i} value={entry.value.replace(/^@LOG=/, '')}/>;
+                    } else {
+                        return <VisualElement key={i} value={entry.value} param={entry.param}/>;
+                    }
+                })}
             </div>
         </div>
     </div>;
