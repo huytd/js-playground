dfed85d12ba69a406d2c3a24e9dd61575091822e
Huy Tran <kingbazoka@gmail.com>
2020-09-02 00:58:04 -0700

    Increase contrast for string visualization

--- src/components/VisualString.tsx
+++ src/components/VisualString.tsx
@@ -15,7 +15,7 @@ export const VisualString = (props) => {
         else return c;
     });
     return <div className={"py-1 px-2 mb-2 bg-red-100 text-red-500 dark-mode:bg-red-900 dark-mode:text-red-300 rounded-md border border-b-2 border-red-500 dark-mode:border-red-600 flex flex-row flex-wrap"}>
-        {chars.length === 1 ? chars[0] : chars.map((c, i) => <div key={i} className={"pt-5 px-2 m-1 relative " + (isMatched(i) ? "bg-red-400 text-white dark-mode:bg-red-700 dark-mode:text-red-200" : "bg-red-200 dark-mode:bg-red-800 text-red-300")}>
+        {chars.length === 1 ? chars[0] : chars.map((c, i) => <div key={i} className={"pt-5 px-2 m-1 relative " + (isMatched(i) ? "bg-red-400 text-white dark-mode:bg-red-700 dark-mode:text-red-200" : "bg-red-200 dark-mode:bg-red-800 text-red-500")}>
             <span className={"absolute top-0 left-0 text-xs ml-1 opacity-50 " + (isMatched(i) ? "text-white" : "")}>{i}</span>
             {c}
         </div>)}
