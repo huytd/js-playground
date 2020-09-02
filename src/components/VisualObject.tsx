263bc27d365da4552955eaa233850ec3567ff4e9
Huy Tran <kingbazoka@gmail.com>
2020-09-02 10:49:39 -0700

    Display full string in visualization

--- src/components/VisualObject.tsx
+++ src/components/VisualObject.tsx
@@ -7,5 +7,5 @@ export const VisualObject = (props) => {
     for (let key in obj) {
         content.push(<div key={Date.now() + key} className={"block p-2"}><div className={"inline-block w-1/12"}>{key}:</div><VisualElement value={obj[key]}/></div>);
     }
-    return <div className={"py-1 px-2 mb-2 bg-orange-100 text-orange-500 dark-mode:bg-mono-700 rounded-md border border-b-2 border-orange-500 dark-mode:border-mono-500 flex flex-row"}> {content} </div>;
+    return <div className={"py-1 px-2 mb-2 bg-orange-100 text-orange-500 dark-mode:bg-mono-700 rounded-md border border-b-2 border-orange-500 dark-mode:border-mono-500 flex flex-row flex-wrap"}> {content} </div>;
 };
--- src/components/VisualString.tsx
+++ src/components/VisualString.tsx
@@ -14,10 +14,13 @@ export const VisualString = (props) => {
         if (c === ' ') return '⠀'; // This is not a space
         else return c;
     });
-    return <div className={"py-1 px-2 mb-2 bg-red-100 text-red-500 dark-mode:bg-red-900 dark-mode:text-red-300 rounded-md border border-b-2 border-red-500 dark-mode:border-red-600 flex flex-row flex-wrap"}>
-        {chars.length === 1 ? chars[0] : chars.map((c, i) => <div key={i} className={"pt-5 px-2 m-1 relative " + (isMatched(i) ? "bg-red-400 text-white dark-mode:bg-red-700 dark-mode:text-red-200" : "bg-red-200 dark-mode:bg-red-800 text-red-500")}>
-            <span className={"absolute top-0 left-0 text-xs ml-1 opacity-50 " + (isMatched(i) ? "text-white" : "")}>{i}</span>
-            {c}
-        </div>)}
+    return <div className={"py-1 px-2 mb-2 bg-red-100 text-red-500 dark-mode:bg-red-900 dark-mode:text-red-300 rounded-md border border-b-2 border-red-500 dark-mode:border-red-600 flex flex-col"}>
+        { chars.length > 1 && (<div className="bg-red-200 text-red-600 m-1 px-2 py-1">"{props.value}"</div>) }
+        <div className="flex flex-row flex-wrap">
+            {chars.length === 1 ? chars[0] : chars.map((c, i) => <div key={i} className={"pt-5 px-2 m-1 relative " + (isMatched(i) ? "bg-red-400 text-white dark-mode:bg-red-700 dark-mode:text-red-200" : "bg-red-200 dark-mode:bg-red-800 text-red-500")}>
+                <span className={"absolute top-0 left-0 text-xs ml-1 opacity-50 " + (isMatched(i) ? "text-white" : "")}>{i}</span>
+                {c}
+            </div>)}
+        </div>
     </div>;
 };
